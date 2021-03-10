10ad419699424a919865ae2765cb2a5f63b1ba51
Khaled Tamer <khaled.t.khairy@gmail.com>
2021-03-10 13:26:24 +0200

    Mobile View Edit

--- src/components/Layout/Toolbar/Toolbar.js
+++ src/components/Layout/Toolbar/Toolbar.js
@@ -3,12 +3,16 @@ import React from'react';
  import Logo from './new clinic logo without name.png';
  import Navigationitems from '../../Navigation/Navigationitems/Navigationitems';
  import Sidebar from '../Sidebar/Sidebar';
+ import {  Link } from 'react-router-dom'; 
 const toolbar =(props)=>(
 <header className={classes.Toolbar}>
     
     <div className={classes.Logo}>
         <img src={Logo} alt="clinic's logo"/>
         
+    </div>
+    <div className={classes.Sidebaronly} className={classes.button}>
+    <Link to="/Register"><button><span>Book Appointment </span></button></Link> 
     </div>
   
     <nav className={classes.Desktoponly} >
